c006da47ac5ede6a373e5a56911f5318c4635858
Josip Igrec <josip@igr.ec>
2024-09-19 21:17:37 +0200

    Add additional data to blog RSS feed

--- src/pages/blog.xml.js
+++ src/pages/blog.xml.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable no-undef */
 import rss from "@astrojs/rss";
 
 import { BLOG_DESCRIPTION, BLOG_TITLE } from "~/app/consts";
@@ -6,15 +10,39 @@ import { getVisibleBlogPosts } from "~/app/helpers";
 export async function GET(context) {
   const posts = await getVisibleBlogPosts();
 
+  const heroImages = new Map();
+
+  for (const post of posts) {
+    const heroImage = post.data.heroImage
+      ? {
+          url: post.data.heroImage.src,
+          type: `image/${post.data.heroImage.format}`,
+          length: 0,
+        }
+      : {
+          url: `/blog/${post.slug}.hero.webp`,
+          type: `image/webp`,
+          length: 0,
+        };
+
+    const heroImageUrl = new URL(heroImage.url, context.site);
+
+    heroImage.url = heroImageUrl.toString();
+
+    heroImages.set(post.id, heroImage);
+  }
+
   return rss({
     title: BLOG_TITLE,
     description: BLOG_DESCRIPTION,
     stylesheet: "/rss/pretty-feed-v3.xsl",
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.slug}/`,
+      author: post.data.author ?? "Josip Igrec <josip@igr.ec>",
+      categories: post.data.tags,
+      enclosure: heroImages.get(post.id),
     })),
   });
 }
